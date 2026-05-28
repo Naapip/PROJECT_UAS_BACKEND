71b0f33bc2ffac71ee5de4148dcee2386046ff38
Naapip <favourty159@gmail.com>
2026-05-28 21:51:24 +0700

    add slice 1 feature create and read nested replies based on mvc

---
+++ uas_backend_threads/app/Models/Reply.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'thread_id', 'parent_reply_id', 'content'];
+
+    // Relasi ke User yang menulis balasan
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi untuk mengambil anak-anak balasan (nested replies) di bawahnya
+    public function childReplies()
+    {
+        return $this->hasMany(Reply::class, 'parent_reply_id')->with('user', 'childReplies');
+    }
+}
